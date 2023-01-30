a2c66ed5ea37b64c980920132b6ef56096b90453
amaia.di <amaia.di@icjardin.com>
2023-01-30 11:27:33 +0100

    Cambios en clase Cambio1 master

--- src/cambio1/Cambio1.java
+++ src/cambio1/Cambio1.java
@@ -3,6 +3,7 @@ package cambio1;
 public class Cambio1 {
 
 	public static void main(String[] args) {
+		System.out.println("Pruebas Git Master. Clase Cambio1");
 		System.out.println("Pruebas Git. Clase Cambio1");
 
 	}
