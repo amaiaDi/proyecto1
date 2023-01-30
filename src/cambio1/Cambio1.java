2c800824acaeee56ded178da8a92283cc7c754cc
amaia.di <amaia.di@icjardin.com>
2023-01-30 11:28:31 +0100

    Cambio en clase Cambio1 en rama cambio1

--- src/cambio1/Cambio1.java
+++ src/cambio1/Cambio1.java
@@ -3,6 +3,7 @@ package cambio1;
 public class Cambio1 {
 
 	public static void main(String[] args) {
+		System.out.println("Pruebas Git Cambio1. Clase Cambio1");
 		System.out.println("Pruebas Git. Clase Cambio1");
 
 	}
